eeda226c295e4b0a40b10452baf7c29f56aced07
Aleksandr Buki <alexandr.buki@videinfra.com>
2026-02-07 18:47:03 +0300

    yandex metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,28 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },
       ],
+      script: [
+        {
+          innerHTML: `
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106706340', 'ym');
+
+            ym(106706340, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `,
+          type: 'text/javascript',
+          defer: true,
+        },
+      ],
+      noscript: [
+        {
+          innerHTML: '<div><img src="https://mc.yandex.ru/watch/106706340" style="position:absolute; left:-9999px;" alt="" /></div>',
+          tagPosition: 'bodyClose'
+        }
+      ]
     },
   },
   runtimeConfig: {
